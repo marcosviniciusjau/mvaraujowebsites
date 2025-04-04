e3e288df265563da3aef138239418f338bcd3c12
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-04-04 14:06:49 -0300

    css

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("eu to aqui");
+const privacyBanner = document.querySelector(".privacy-banner");
+
+privacyBanner.addEventListener("click", () => {
+  privacyBanner.classList.add("esconder");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const acceptBtn = document.getElementById("accept-privacy");
+
+  if (localStorage.getItem("acceptedPrivacy") === "true") {
+    privacyBanner.classList.add("esconder");
+  }
+
+  acceptBtn.addEventListener("click", () => {
+    localStorage.setItem("acceptedPrivacy", "true");
+    privacyBanner.classList.add("esconder");
+  });
+});
